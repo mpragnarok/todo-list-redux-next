9c19fa3effe25665af972904bf8fb629686ff3a7
minaH <mp922352612@gmail.com>
2020-03-14 16:25:47 +0800

    Create CreateTodo layout

--- components/CreateTodo.js
+++ components/CreateTodo.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Input, Button } from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
 
 class CreateTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoText: ""
+    };
+    this.onChangeTodoText = this.onChangeTodoText.bind(this);
+  }
+
+  onChangeTodoText(e) {
+    this.setState({
+      todoText: e.target.value
+    });
+  }
   render() {
     return (
       <div className="create-todo">
         <Row gutter={[16, 16]} justify="center" align="middle">
-          <Col>
-            <h1>Create Todos</h1>
+          <h1>Create Todos</h1>
+        </Row>
+        <Row gutter={[16, 16]} justify="center" align="middle">
+          <Col span={8}>
+            <Input placeholder="Create todo here"></Input>
+          </Col>
+          <Col span={4}>
+            <Button danger type="primary">
+              Cancel
+            </Button>{" "}
+            <Button type="primary" icon={<PlusCircleOutlined />}>
+              Add Todo
+            </Button>
           </Col>
         </Row>
 
