c6dc85593f36fba0079a73ce1a679d307ecb2b52
mina.h <mp922352612@gmail.com>
2020-03-16 17:52:30 +0800

    Create Todo, Filter reducers and combine reudcers

--- store/index.js
+++ store/index.js
@@ -2,6 +2,18 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import Reducers from "./reducers";
 
+// initial data
+// const INITIAL_DATA = [
+//   {
+//     id: 0,
+//     text: 'Walk the Dog'
+//   },
+//   {
+//     id: 1,
+//     text: 'Learn Redux'
+//   }
+// ]
+
 export const initializeStore = () => {
   return createStore(Reducers, composeWithDevTools(applyMiddleware()));
 };
--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,6 +1,9 @@
 import {combineReducers} from redux;
-
-const rootReducer = combineReducers()
+import todos from './TodoReducer'
+import visibilityFilter from './FilterReducer'
+const rootReducer = combineReducers({
+  todos, visibilityFilter
+})
 
 
 export default rootReducer;
