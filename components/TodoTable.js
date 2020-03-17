0a99b97d9213f44365a79a241a69a86384695b2b
mina.h <mp922352612@gmail.com>
2020-03-17 11:45:26 +0800

    Get initial todos from store

--- components/TodoTable.js
+++ components/TodoTable.js
@@ -1,31 +1,20 @@
 import { Breadcrumb, Table, Row, Col, Button } from "antd";
 import { CheckOutlined, MinusOutlined } from "@ant-design/icons";
 const { Column, ColumnGroup } = Table;
+import { connect } from "react-redux";
 import React from "react";
 
 import "../public/static/sass/TodoTable.scss";
 class TodoTable extends React.Component {
+  state = {
+    todos: this.props.todos
+  };
   render() {
-    const data = [
-      {
-        key: "1",
-        todos: "Make dinner"
-      },
-      {
-        key: "2",
-        todos: "Finish todo list side project"
-      },
-      {
-        key: "3",
-        todos: "Look up SASS doc"
-      }
-    ];
-
     return (
       <div className="todo-table">
         <Row gutter={[16, 16]} justify="center" align="middle">
           <Col span={12}>
-            <Table dataSource={data}>
+            <Table dataSource={this.state.todos}>
               <Column title="Todos" dataIndex="todos" key="todos" />
               <Column
                 title="Actions"
@@ -49,4 +38,10 @@ class TodoTable extends React.Component {
   }
 }
 
-export default TodoTable;
+function mapStateToProps(state) {
+  return {
+    todos: state.todos
+  };
+}
+
+export default connect(mapStateToProps)(TodoTable);
